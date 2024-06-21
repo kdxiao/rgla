a7ff6d2268a1f4cf306cd379f0b7f393d49cc900
kdxiao <kdxiao0203@gmail.com>
2024-06-20 20:04:15 -0400

    changed to library

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+pub mod vec2;
+pub use self::vec2::*;
+
+pub mod vec3;
+pub use self::vec3::*;
+
+pub mod vec4;
+pub use self::vec4::*;
+
+pub mod mat2;
+pub use self::mat2::*;
+
+pub mod mat3;
+pub use self::mat3::*;
+
+pub mod mat4;
+pub use self::mat4::*;
+
+pub mod bivec;
+pub use self::bivec::*;
+
+pub mod rotor;
+pub use self::rotor::*;
+
+
